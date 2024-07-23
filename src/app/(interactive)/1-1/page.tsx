891709cec5336f32df7fdec4d09af8d524ffad9e
matikant <fe-2003@hotmail.com>
2024-07-23 23:33:10 +0700

    refactor: remove pages

--- src/app/(interactive)/1-1/page.tsx
+++ src/app/(interactive)/1-1/page.tsx
@@ -1,39 +1,15 @@
 "use client";
-import { motion } from "framer-motion";
-import Image from "next/image";
-import Link from "next/link";
+import { useState } from "react";
 import { MovingSky } from "~/component/bg/MovingSky";
-import { NextButton } from "~/component/NextButton";
+import Text from "./text";
 
 export default function Page() {
+  const [state, setState] = useState<number>(1);
+
   return (
     <div className="overflow-hidden object-none">
       <MovingSky />
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{
-          duration: 1,
-          delay: 0.5,
-        }}
-        className="flex min-h-screen flex-col"
-      >
-        <div className="fixed z-10 flex items-center justify-center">
-          <Image
-            src="/img/ysym_logo.webp"
-            alt="yourskyyourmind logo"
-            width={1080}
-            height={1920}
-            className="h-auto w-screen overflow-hidden"
-          />
-        </div>
-
-        <Link href="/1-2">
-          <div className="fixed bottom-32 z-20 flex w-screen justify-center">
-            <NextButton />
-          </div>
-        </Link>
-      </motion.div>
+      <Text state={state} setState={setState}></Text>
     </div>
   );
 }
