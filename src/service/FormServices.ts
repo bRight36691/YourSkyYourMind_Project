67ef2d825ef8f4eeb04674c6e4795e2f8dc8771e
bRight36691 <141995847+bRight36691@users.noreply.github.com>
2024-08-04 00:41:02 +0700

    Update FormServices.ts

--- src/service/FormServices.ts
+++ src/service/FormServices.ts
@@ -5,26 +5,14 @@ interface BurnoutScoreResponse {
   data: string; // Adjust the type based on actual API response
 }
 
-const isBurnoutScoreResponse = (response: any): response is BurnoutScoreResponse => {
-  return response && typeof response.data === 'string';
-};
-
-
 export const FormServices = {
   getAvgBurnoutScore: async (): Promise<string> => {
     try {
       const res = await fetch(
         `${env.NEXT_PUBLIC_BACKEND_URL}/api/v1/form/burnout`,
       );
-      const data = await res.json();
-
-      // Validate and ensure data conforms to the expected type
-      if (isBurnoutScoreResponse(data)) {
-        return data.data;
-      } else {
-        console.error('Unexpected response format', data);
-        return "";
-      }
+      const data:BurnoutScoreResponse = await res.json();
+      return data.data;
     } catch (error) {
       return "";
     }
