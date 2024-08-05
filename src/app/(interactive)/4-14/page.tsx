7c9b10ec182c94e33b8b363077f6107a2f61e50a
bRight36691 <141995847+bRight36691@users.noreply.github.com>
2024-08-05 19:53:48 +0700

    fix: scene4-14

--- src/app/(interactive)/4-14/page.tsx
+++ src/app/(interactive)/4-14/page.tsx
@@ -5,8 +5,6 @@ import Link from "next/link";
 
 export default function Page() {
     
-
-   
     return (
         <AnimatePresence>
             <motion.div 
@@ -14,17 +12,14 @@ export default function Page() {
                 animate={{opacity:1}}
                 exit={{opacity:1 , transition:{duration:0.5}}}
                 transition={{duration:2,delay:1}}
-                className="grid justify-items-center  text-black min-h-screen min-w-full bg-[#fbfbfb] ">
-                <div className="grid justify-items-center h-[10%] mt-[40%] ">
-                    <h1>บนหน้าจอ มีข้อความหนึ่งเขียนอยู่</h1>
-                </div>
+                className="grid justify-items-center  text-black min-h-screen min-w-full bg-[#fbfbfb] ">    
+                <h1 className="absolute top-24">บนหน้าจอ มีข้อความหนึ่งเขียนอยู่</h1>
                 <Link className="fixed z-50 bottom-16 left-[36%]" href='/4-15'>
                     <button className=" bg-white text-lg h-8 w-28 rounded-2xl shadow-xl" >ถัดไป</button>
                 </Link>
                 <img 
                     src="/img/phone.webp" 
                     className="absolute bottom-[27%] left-[18%] w-[250px]"
-                    
                 />
 
             </motion.div>
