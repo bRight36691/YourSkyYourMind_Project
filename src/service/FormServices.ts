3a5e27fbbc924d7e8ce263190555de7863d6d634
matikant <fe-2003@hotmail.com>
2024-08-06 21:39:17 +0700

    fix: fetch average burnout score

--- src/service/FormServices.ts
+++ src/service/FormServices.ts
@@ -6,30 +6,34 @@ interface BurnoutScoreResponse {
 }
 
 // Type guard to validate the response
-const isBurnoutScoreResponse = (response: unknown): response is BurnoutScoreResponse => {
+const isBurnoutScoreResponse = (
+  response: unknown,
+): response is BurnoutScoreResponse => {
   return (
-    typeof response === 'object' &&
+    typeof response === "object" &&
     response !== null &&
-    'data' in response &&
-    typeof (response as BurnoutScoreResponse).data === 'string'
+    "data" in response &&
+    typeof (response as BurnoutScoreResponse).data === "number"
   );
 };
 
 export const FormServices = {
   getAvgBurnoutScore: async (): Promise<string> => {
     try {
-      const res = await fetch(`${env.NEXT_PUBLIC_BACKEND_URL}/api/v1/form/burnout`);
+      const res = await fetch(
+        `${env.NEXT_PUBLIC_BACKEND_URL}/api/v1/form/burnout`,
+      );
       const data: unknown = await res.json();
 
       // Validate and ensure data conforms to the expected type
       if (isBurnoutScoreResponse(data)) {
         return data.data;
       } else {
-        console.error('Unexpected response format', data);
+        console.error("Unexpected response format", data);
         return "";
       }
     } catch (error) {
-      console.error('Error fetching burnout score', error);
+      console.error("Error fetching burnout score", error);
       return "";
     }
   },
@@ -44,7 +48,7 @@ export const FormServices = {
         body: JSON.stringify(form),
       });
     } catch (error) {
-      console.error('Error creating form', error);
+      console.error("Error creating form", error);
     }
   },
 };
