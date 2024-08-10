8aac177403e16602cc69afcc8cdcfa67e5df8837
matikant <fe-2003@hotmail.com>
2024-08-10 19:58:43 +0700

    fix: big screen scene 2

--- src/app/(interactive)/2-6/page.tsx
+++ src/app/(interactive)/2-6/page.tsx
@@ -8,13 +8,13 @@ export default function Page() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 2, delay: 0.5 }}
-      className="grid h-screen w-screen justify-items-center bg-[#fbfbfb]"
+      className="grid h-screen w-full justify-items-center bg-[#fbfbfb]"
     >
       <div className="pt-[60%] text-center text-[20px]">
         <h1>แล้วเรื่องเพื่อน ๆ ล่ะ ?</h1>
       </div>
-      <img src="/img/friends.GIF" className="fixed"></img>
-      <Link className="fixed bottom-8 left-[36%] z-50" href="/2-7">
+      <img src="/img/friends.GIF" className="absolute"></img>
+      <Link className="absolute bottom-8 left-[36%] z-50" href="/2-7">
         <button className="h-8 w-28 rounded-2xl bg-white text-lg shadow-xl">
           ถัดไป
         </button>
