da306fafc35750cded9270afa863d84a707a8a6a
matikant <fe-2003@hotmail.com>
2024-08-20 13:07:15 +0700

    fix: remove non med form submission

--- src/app/(interactive)/3-20/page.tsx
+++ src/app/(interactive)/3-20/page.tsx
@@ -3,19 +3,18 @@
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { FormMappers } from "~/service/FormMappers";
-import { FormServices } from "~/service/FormServices";
+// import { FormMappers } from "~/service/FormMappers";
+// import { FormServices } from "~/service/FormServices";
 
 export default function Page() {
-
   const [isMedStudent, setIsMedStudent] = useState<string>();
   useEffect(() => {
     setIsMedStudent(localStorage.getItem("isMedStudent") ?? "");
   }, []);
 
-  const submitNonMedForm = async () => {
-    void FormServices.createForm(await FormMappers.mapForm());
-  };
+  // const submitNonMedForm = async () => {
+  //   void FormServices.createForm(await FormMappers.mapForm());
+  // };
 
   return (
     <motion.div
@@ -41,7 +40,7 @@ export default function Page() {
         <Link className="fixed bottom-10 left-[36%] z-50" href="/4-1">
           <button
             className="h-8 w-28 rounded-2xl bg-white text-lg shadow-xl"
-            onClick={submitNonMedForm}
+            // onClick={submitNonMedForm}
           >
             ถัดไป
           </button>
