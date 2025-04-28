62aaa8950fc189df1e05393ff4bc02af15e837b5
matikant <fe-2003@hotmail.com>
2025-04-28 21:19:44 +0700

    remove unused import

--- src/app/(interactive)/4-22/page.tsx
+++ src/app/(interactive)/4-22/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { motion } from "framer-motion";
-import Link from "next/link";
 import { useEffect, useState } from "react";
 import { ResultIGTemplate2 } from "~/component/ResultIGTemplate2";
 
